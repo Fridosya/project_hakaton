2e695aa975c55693c930ee398444e07867c9f23b
aerokiller3 <kirillceeb@gmail.com>
2022-11-05 00:38:16 +0500

    first commit

---
+++ frontend/src/views/auth/SignIn.vue
@@ -0,0 +1,12 @@
+<template>
+   <SignInForm/>
+</template>
+
+<script>
+import SignInForm from '@/components/auth/SignInForm.vue'
+
+export default{
+   name: "SignIn",
+   components:{ SignInForm}
+}
+</script>
